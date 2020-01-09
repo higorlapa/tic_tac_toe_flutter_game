ca77b6a9f225579ed5ddd243f1fe310f8c95edcf
Higor Lapa <higor@lotadata.com>
2020-01-09 13:12:24 -0300

    finish the x_widget

--- lib/ui/widgets/x_widget.dart
+++ lib/ui/widgets/x_widget.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class XWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: 20,
+      height: 20,
+      child: Icon(Icons.close),
+    );
   }
 }
