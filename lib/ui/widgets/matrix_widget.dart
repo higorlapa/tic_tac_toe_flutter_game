ef7c42ecd508a5b6266e84aa8667fd4d66717013
Higor Lapa <higor@lotadata.com>
2020-01-09 14:08:45 -0300

    removes the static widget and upgrades the dart sdk env

--- lib/ui/widgets/matrix_widget.dart
+++ lib/ui/widgets/matrix_widget.dart
@@ -13,6 +13,10 @@ class _MatrixWidgetState extends State<MatrixWidget> {
   final GlobalKey _rowKey = GlobalKey();
   double _rowHeight = 10;
 
+  List<Widget> _rowOneList = List();
+  List<Widget> _rowTwoList = List();
+  List<Widget> _rowThreeList = List();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,39 +25,18 @@ class _MatrixWidgetState extends State<MatrixWidget> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Row(
-            key: _rowKey,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              XWidget(),
-              _verticalWidget(),
-              XWidget(),
-              _verticalWidget(),
-              XWidget(),
-            ],
-          ),
+              key: _rowKey,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: _rowOneList),
           Divider(),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              XWidget(),
-              _verticalWidget(),
-              XWidget(),
-              _verticalWidget(),
-              CircleWidget(isDark: false)
-            ],
-          ),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: _rowTwoList),
           VerticalDivider(),
           Divider(),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              XWidget(),
-              _verticalWidget(),
-              CircleWidget(isDark: false),
-              _verticalWidget(),
-              XWidget()
-            ],
-          )
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: _rowThreeList)
         ],
       ),
     );
@@ -66,12 +49,10 @@ class _MatrixWidgetState extends State<MatrixWidget> {
     SchedulerBinding.instance.addPostFrameCallback((_) {
       var brightness = MediaQuery.of(context).platformBrightness;
 
-      _isDark = brightness == Brightness.dark;
-    });
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
       setState(() {
+        _isDark = brightness == Brightness.dark;
         _rowHeight = _rowKey.currentContext.size.height;
+        _initLists();
       });
     });
   }
@@ -79,5 +60,21 @@ class _MatrixWidgetState extends State<MatrixWidget> {
   Widget _verticalWidget() {
     return Container(
         height: _rowHeight, child: VerticalDivider(color: Colors.grey));
+  }
+
+  _initLists() {
+    /// Positions 0,2,4
+
+    List<Widget> defaultList = [
+      Container(height: 30, width: 30),
+      _verticalWidget(),
+      Container(height: 30, width: 30),
+      _verticalWidget(),
+      Container(height: 30, width: 30)
+    ];
+
+    _rowOneList = [...defaultList];
+    _rowTwoList = [...defaultList];
+    _rowThreeList = [...defaultList];
   }
 }
